7bc54b62d9d0dfa8915f920e6156e922e2a17736
Ortho Lab <ortho@ortho-3138.local>
2019-04-22 17:30:43 -0500

    Quiz Generator: Main Menu with background

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,12 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -22,24 +28,37 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			Image quizBackground = new Image("background3.png", 400, 400, false, true);
 			
-            Button button1 = new Button("Load/Import Questions");
-            Button button2 = new Button("Add Questions");
-            Button button3 = new Button("Start Quiz");
-            Button button4 = new Button("Exit");
-            VBox vbox = new VBox(button1, button2, button3, button4);
+			
+			
+			   BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+
+			   Background background = new Background(new BackgroundImage(quizBackground,
+		            BackgroundRepeat.NO_REPEAT,
+		            BackgroundRepeat.NO_REPEAT,
+		            BackgroundPosition.CENTER,
+		            bSize));
+			   
+			   
+			   root.setBackground(background);
+
+
+			
+            Button buttonLoadImport = new Button("Load/Import Questions");
+            Button buttonAddQuestion = new Button("Add Questions");
+            Button buttonStartQuiz = new Button("Start Quiz");
+            Button buttonExit = new Button("Exit");
+            VBox vbox = new VBox(buttonLoadImport, buttonAddQuestion, buttonStartQuiz, buttonExit);
             vbox.setAlignment(Pos.CENTER);
             vbox.setSpacing(40);
             
             int totalNumQuestions = 0;
             Label reportTotalQuestions = new Label("There are " + totalNumQuestions + " questions in the database");
-           
-            //reportTotalQuestions.setAlignment(Pos.CENTER);
+  
             root.setBottom(reportTotalQuestions);
             root.setAlignment(reportTotalQuestions, Pos.CENTER);
      
-            
-            
             root.setCenter(vbox);
 			
 		} catch(Exception e) {
@@ -51,5 +70,3 @@ public class Main extends Application {
 		launch(args);
 	}
 }
-
-
