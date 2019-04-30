ebd3eecaf2fb4fc744a77379040330b85d1efc0c
Ian Ruh <ianruh2@gmail.com>
2019-04-29 20:17:56 -0500

    Quiz to single question at a time

--- src/application/BasicQuiz.java
+++ src/application/BasicQuiz.java
@@ -3,7 +3,6 @@ import java.util.List;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -12,7 +11,7 @@ import javafx.scene.layout.VBox;
  * @author ianruh
  *
  */
-public class BasicQuiz extends ScrollPane implements Quiz {
+public class BasicQuiz extends VBox implements Quiz {
 	
 	/**
 	 * The ResultsBox class is convenience class to contain the results box.
@@ -61,9 +60,31 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 			buttons.getChildren().add(saveQuizButton);
 			
 			this.getStyleClass().add("question-card");
+			this.setMaxWidth(500);
 			
 			this.getChildren().add(labels);
 			this.getChildren().add(buttons);
+		}
+	}
+	
+	/**
+	 * Class that encapsulates a bar under each question.
+	 * @author ianruh
+	 *
+	 */
+	private class NextBar extends HBox {
+		/**
+		 * Basic constructor that takes a handler for the next button.
+		 * @param nextHandler
+		 */
+		private NextBar(EventHandler nextHandler) {
+			super(15);
+			Button nextButton = new Button("Next");
+			nextButton.setOnMouseClicked((e) -> nextHandler.handleEvent());
+			nextButton.getStyleClass().add("finish-quiz-items");
+			this.getStyleClass().add("question-card");
+			this.setMaxWidth(500);
+			this.getChildren().add(nextButton);
 		}
 	}
 	
@@ -75,12 +96,15 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 	// Store a list of the questions.
 	protected List<BasicQuestion> questions;
 	
-	// The vertical box used for the layout
-	private VBox verticalBox;
-	
 	// A storage reference for the event handlers
 	private EventHandler finishHandler;
 	private EventHandler newQuizHandler;
+	
+	// The single next question bar
+	NextBar nextBar = new NextBar(() -> this.nextQuestion());
+	
+	// The single results bar
+	ResultsBox resultsBox = null;
 	
 	
 	/**
@@ -89,11 +113,7 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 	 * @param finishHandler Handler for what to do when the quiz is completed.
 	 */
 	public BasicQuiz(List<BasicQuestion> questions, EventHandler finishHandler, EventHandler newQuizHandler) {
-		super();
-		
-		//Initialize the vertical box
-		this.verticalBox = new VBox(20);
-		
+		super(15);
 		// Store the questions.
 		this.questions = questions;
 		
@@ -101,28 +121,25 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 		this.finishHandler = finishHandler;
 		this.newQuizHandler = newQuizHandler;
 		
-		// Add the Quiz title
-		Label quizLabel = new Label("Quiz");
-		quizLabel.getStyleClass().add("quiz-title");
-		this.verticalBox.getChildren().add(quizLabel);
-		
 		// Set the properties
-		this.setContent(this.verticalBox);
 		this.getStyleClass().add("quiz-container");
 		
 		// Add the questions to the vertical box.
-		addQuestion();
+		nextQuestion();
 		
 	}
 	
 	/**
 	 * Adds all of the questions to the vertical box.
 	 */
-	private void addQuestion() {
+	private void nextQuestion() {
+		this.getChildren().clear();
 		final int INDEX = this.numQuestionsShowing;
 		this.questions.get(this.numQuestionsShowing).addAnsweredHandler(() -> checkAllAnswered(INDEX));
-		this.verticalBox.getChildren().add(this.questions.get(this.numQuestionsShowing));
+		this.getChildren().add(this.questions.get(this.numQuestionsShowing));
 		this.numQuestionsShowing++;
+		this.getChildren().add(nextBar);
+		nextBar.setVisible(false);
 	}
 
 	/**
@@ -137,11 +154,15 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 	 * Check if all of the questions have been answered, and if so, show the results.
 	 */
 	private void checkAllAnswered(int index) {
+		// Check if the next button should be shown.
+		if(index != this.questions.size()-1)
+			nextBar.setVisible(true);
+		
 		// Check that all the questions have been answered.
 		boolean allAnswered = this.questions.size()-1 == index;
 		
 		// If all have been answered, display the results box
-		if(allAnswered) {
+		if(allAnswered && this.resultsBox == null) {
 			for(int i = 0; i < this.questions.size(); i++) {
 				if(this.questions.get(i).answeredCorrectly) {
 					this.numCorrect++;
@@ -166,15 +187,14 @@ public class BasicQuiz extends ScrollPane implements Quiz {
 				this.newQuizHandler.handleEvent();
 			};
 			
-			ResultsBox resultsBox = new ResultsBox(this.numCorrect, 
+			this.resultsBox = new ResultsBox(this.numCorrect, 
 					(this.numWrong + this.numCorrect), 
 					resetAndFinishHandler, 
 					resetNewQuizHandler, 
 					saveQuizHandler);
 			
-			this.verticalBox.getChildren().add(resultsBox);
-		} else if(index == this.numQuestionsShowing-1) {
-			this.addQuestion();
+			this.getChildren().remove(this.nextBar);
+			this.getChildren().add(resultsBox);
 		}
 	}
 	
