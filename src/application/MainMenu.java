c657767a6c64d03422dee1e98794176e5c2454c9
PrestonLewis <prestonlewis2017@gmail.com>
2019-05-01 18:57:01 -0500

    merge

--- src/application/MainMenu.java
+++ src/application/MainMenu.java
@@ -5,7 +5,7 @@
  * Semester:   Spring 2019
  * Course:     CS400
  * 
- * @author Preston Lewis, ID 9074531329, prlewis@wisc.edu, lecture 004      
+ * @author Preston Lewis, ID 9074531329, prlewis@wisc.edu, lecture 004 
  * @author Jared Krahn, ID 9076949693, jkrahn2@wisc.edu, lecture 004
  * @author Ian Ruh, ID 9080231591, iruh@wisc.edu, lecture 004
  * @author Emily Binversie ID 9063469945, eebinversie@wisc.edu, lecture 004
